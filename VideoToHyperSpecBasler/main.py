60dea612eb241760c5f8252747de1ab7723f289a
Karl Kuckelsberg <karl_ruben.kuckelsberg@hsbi.de>
2023-06-19 17:51:15 +0200

    Added White  Cube Calibration

--- VideoToHyperSpecBasler/main.py
+++ VideoToHyperSpecBasler/main.py
@@ -6,24 +6,25 @@ from pypylon import genicam
 
 
 
-def create_Metadata(h :int, w :int, frames :int):
+def create_Metadata(h :int, w :int, frames :int,camera):
     meta = {}
     meta["description"] = "Hyper X1 (Prototype) Basler"
-    meta["samples"] = h
+    meta["samples"] = camera.Height.GetValue()
     meta["lines"] = frames
-    meta["bands"] = w
+    #meta["bands"] = camera.Width.GetValue()
+    meta["bands"] = 1920
     meta["spectral binning"] = 1
     meta["interleave"] = "bil"
-    meta["bit depth"] = 10
+    meta["bit depth"] = 0
     meta["header offset"] = 0
-    meta["framerate"] = 30
-    meta["shutter"] = 1 / 25
-    meta["gain"] = 1
-    meta["imager type"] = "RaspiCam HQ"
-    meta["imager serial number"] = "001"
+    meta["framerate"] = camera.ResultingFrameRate.GetValue()
+    meta["shutter"] = camera.ExposureTime.GetValue()
+    meta["gain"] = camera.Gain.GetValue()
+    meta["imager type"] = camera.GetDeviceInfo().GetModelName()
+    meta["imager serial number"] = camera.GetDeviceInfo().GetSerialNumber()
     meta["wavelength units"] = "nm"
     meta["wavelength"] = create_BandInfo(w)
-    meta["reflectance scale factor"] = 1
+    meta["reflectance scale factor"] = 0
     print("Metadata created!")
     return meta
 
@@ -49,11 +50,13 @@ def initCamera():
     camera.Open()
     camera.Height.SetValue(996)
     camera.OffsetY.SetValue(104)
+    camera.ExposureTime.SetValue(1219994)
+    camera.PixelFormat.SetValue("Mono12p")
+    camera.Gain.SetValue(0)
+    camera.BinningHorizontal.SetValue(1)
     width = camera.Width.GetValue()
     height = camera.Height.GetValue()
-    camera.ExposureTime.SetValue(250000)
-    camera.PixelFormat.SetValue("Mono12")
-    camera.Gain.SetValue(0)
+    print(f'Binning: {camera.BinningVertical.GetValue}')
     print(f'Gain: {camera.Gain.GetValue()}')
     print(f'Height: {height}')
     print(f'Width: {width}')
@@ -65,30 +68,39 @@ def initCamera():
 
 path = "C:/Users/kkuckelsberg/HyperSpec"
 
-filename = "/Hallogen"
+filename = "/Test"
 
 camera = initCamera()
 
 imgArray = []
 
-numberOfImagesToGrab = 40
+numberOfImagesToGrab = 10
 camera.StartGrabbingMax(numberOfImagesToGrab)
 
+ref = cv.imread("Teflon_muster.png",cv.IMREAD_UNCHANGED)
 
+print(f'DataType ReferenzImg: {ref.dtype}')
+
+ref = np.uint16((ref[:,:,0]/255)*4095)
+ref = np.clip(ref,1,np.amax(ref))
 
 while camera.IsGrabbing():
     grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
     if grabResult.GrabSucceeded():
         img = grabResult.Array
+        print(f'max Img: {np.amax(img)}, max ref: {np.amax(ref)}')
+        img = np.divide(img, ref)*1000
+        #img = np.clip(img,0,10000)
         imgArray.append(img)
     grabResult.Release()
 
-camera.Close()
+
 
 imgArray = np.stack(imgArray, axis=0)  # dimensions (T, H, W, C)
 
 print(f'output datatyp: {imgArray.dtype}')
 
 #convert 4D Numpy Matrix to Hyperspac file
-envi.save_image(f"{path}/Bil{filename}.hdr", imgArray, metadata=create_Metadata(imgArray[0].shape[0], imgArray[0].shape[1], imgArray.shape[0]), force=True, ext="bil",dtype=np.uint16)
+envi.save_image(f"{path}/Bil{filename}.hdr", imgArray, metadata=create_Metadata(imgArray[0].shape[0], imgArray[0].shape[1], imgArray.shape[0],camera), force=True, ext="bil",dtype=np.uint16)
 print("HyperSpactral Image saved successfully in {}".format(f"{path}/Bil{filename}.bil"))
+camera.Close()
