d2e1548a42708e25a7ab7b18e55272b800df4f1e
Karl Kuckelsberg <karl_ruben.kuckelsberg@hsbi.de>
2023-06-19 10:56:50 +0200

    integrated Bassler Camera

--- VideoToHyperSpecBasler/main.py
+++ VideoToHyperSpecBasler/main.py
@@ -1,4 +1,4 @@
-import cv2.cv2 as cv
+import cv2.cv2 as cv #version 4.5.562
 import numpy as np
 from spectral import envi
 from pypylon import pylon           # version 1.9
@@ -8,7 +8,7 @@ from pypylon import genicam
 
 def create_Metadata(h :int, w :int, frames :int):
     meta = {}
-    meta["description"] = "Hyper X1 (Prototype)"
+    meta["description"] = "Hyper X1 (Prototype) Basler"
     meta["samples"] = h
     meta["lines"] = frames
     meta["bands"] = w
@@ -47,14 +47,23 @@ def initCamera():
 
     camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
     camera.Open()
+    camera.Height.SetValue(996)
+    camera.OffsetY.SetValue(104)
     width = camera.Width.GetValue()
     height = camera.Height.GetValue()
-    camera.ExposureTime.SetValue(200000)
-    print(camera.ExposureTime.GetValue())
-    print(camera.ResultingFrameRate.GetValue())
+    camera.ExposureTime.SetValue(250000)
+    camera.PixelFormat.SetValue("Mono12")
+    camera.Gain.SetValue(0)
+    print(f'Gain: {camera.Gain.GetValue()}')
+    print(f'Height: {height}')
+    print(f'Width: {width}')
+    print(f'OffsetY: {camera.OffsetY.GetValue()}')
+    print("Exposuretime:. ".format(camera.ExposureTime.GetValue()))
+    print("Framerat: {}".format(camera.ResultingFrameRate.GetValue()))
+    print("Pixeltype: {}".format(camera.PixelFormat.GetValue()))
     return camera
 
-path = "/Users/karlkuckelsberg/Desktop/HyperSpec"
+path = "C:/Users/kkuckelsberg/HyperSpec"
 filename = "/first_capture_basler"
 vid_name = "/first_capture_basler.mp4"
 
@@ -62,17 +71,18 @@ camera = initCamera()
 
 imgArray = []
 
-numberOfImagesToGrab = 100
+numberOfImagesToGrab = 40
 camera.StartGrabbingMax(numberOfImagesToGrab)
+
+
 
 while camera.IsGrabbing():
     grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
     if grabResult.GrabSucceeded():
         img = grabResult.Array
         imgArray.append(img)
-
-
     grabResult.Release()
+
 camera.Close()
 
 imgArray = np.stack(imgArray, axis=0)  # dimensions (T, H, W, C)
